42fdeabe2aae23c13a6008f087dca455d4e129c7
Angry Wasp <angrywasp@live.com>
2019-07-15 20:50:40 +1000

    Add new endpoint AddPeer

---
+++ Src/Nerva.Rpc/Daemon/AddPeer.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nerva.Rpc.Daemon
+{
+    public class AddPeer : Request<AddPeerRequestData, string>
+    {
+        public AddPeer(AddPeerRequestData rpcData, Action<string> completeAction, Action<RequestError> failedAction, 
+            string host = Config.DEFAULT_HOST, uint port = Config.DEFAULT_PORT, Log log = null)
+            : base (rpcData, completeAction, failedAction, host, port, log) { }
+
+        protected override bool DoRequest(out string result)
+        {
+            bool r = JsonRpcRequest("add_peer", rpcData, out result);
+            return r;
+        }
+    }
+
+    [JsonObject]
+    public class AddPeerRequestData
+    {
+        [JsonProperty("host")]
+        public string Host { get; set; }
+    }
+}
