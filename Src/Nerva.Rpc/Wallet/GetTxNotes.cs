7cb36a06bcceac776210c1eb8bd35c36c3f7a034
Angry Wasp <angrywasp@live.com>
2020-10-23 18:34:15 +1100

    Add end points to get and set transaction notes

---
+++ Src/Nerva.Rpc/Wallet/GetTxNotes.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nerva.Rpc.Wallet
+{
+    public class GetTxNotes : Request<GetTxNotesRequestData, GetTxNotesResponseData>
+    {
+        public GetTxNotes(GetTxNotesRequestData rpcData, Action<GetTxNotesResponseData> completeAction, Action<RequestError> failedAction,
+            string host = Config.DEFAULT_HOST, uint port = Config.DEFAULT_PORT, Log log = null)
+            : base (rpcData, completeAction, failedAction, host, port, log) { }
+            
+        protected override bool DoRequest(out GetTxNotesResponseData result)
+        {
+            string json = null;
+            bool r = JsonRpcRequest("get_tx_notes", rpcData, out json);
+            result = r ? JsonConvert.DeserializeObject<ResponseData<GetTxNotesResponseData>>(json).Result : null;
+            return r;
+        }
+    }
+
+    [JsonObject]
+    public class GetTxNotesRequestData
+    {
+        [JsonProperty("txids")]
+        public List<string> TxIds { get; set; } = new List<string>();
+    }
+
+    [JsonObject]
+    public class GetTxNotesResponseData
+    {
+        [JsonProperty("notes")]
+        public List<string> Notes { get; set; } = new List<string>();
+    }
+}
---
+++ Src/Nerva.Rpc/Wallet/SetTxNotes.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nerva.Rpc.Wallet
+{
+    public class SetTxNotes : Request<SetTxNotesRequestData, string>
+    {
+        public SetTxNotes(SetTxNotesRequestData rpcData, Action<string> completeAction, Action<RequestError> failedAction,
+            string host = Config.DEFAULT_HOST, uint port = Config.DEFAULT_PORT, Log log = null)
+            : base (rpcData, completeAction, failedAction, host, port, log) { }
+            
+        protected override bool DoRequest(out string result) => JsonRpcRequest("set_tx_notes", rpcData, out result);
+    }
+
+    [JsonObject]
+    public class SetTxNotesRequestData
+    {
+        [JsonProperty("txids")]
+        public List<string> TxIds { get; set; } = new List<string>();
+
+        [JsonProperty("notes")]
+        public List<string> Notes { get; set; } = new List<string>();
+    }
+}
